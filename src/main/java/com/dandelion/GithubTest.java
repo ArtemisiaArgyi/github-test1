8a067f5dd69078962118d2021469915bea0880f3
dandelion <dandelion@qq.com>
2022-05-25 16:55:41 +0800

    在IDEA中第二次提交

--- src/main/java/com/dandelion/GithubTest.java
+++ src/main/java/com/dandelion/GithubTest.java
@@ -3,5 +3,6 @@ package com.dandelion;
 public class GithubTest {
     public static void main(String[] args) {
         System.out.println("Hello,World!");
+        System.out.println("Hello,World2!");
     }
 }
